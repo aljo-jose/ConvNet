1e3f914471e5905db91764d05f66373cc0bd011d
Aljo Jose <aljo@Aljos-MBP.Home>
2020-07-11 12:24:11 +0400

    image classifier evaluation, with gpu enabled code.

--- image_classification_pytorch.py
+++ image_classification_pytorch.py
@@ -126,8 +126,54 @@ net.load_state_dict(torch.load(PATH))
 
 outputs = net(images) # calls net.forward inetrnally.
 
-_, predicted = torch.max(outputs, 1)
+_, predicted = torch.max(outputs,1)
 
 print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
-
         
+"""
+test on complete dataset.
+"""
+total = 0
+correct = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+
+"""
+performance on each classes.
+"""
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
+
+"""
+train on gpu.
+"""
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+net.to(device) # copy network to gpu.
+inputs, labels = data[0].to(device), data[1].to(device)
+
